1aa4fc87504217c4a6dca4fdc650547d0215702d
Cristhiano Roberto Carvalho Pinto <crpinto1@stefanini.com>
2018-02-27 13:47:27 -0300

    Modificando ItemResource para CORS multimplos

--- src/main/java/com/herokuapp/patrimonioapi/resource/ItemResource.java
+++ src/main/java/com/herokuapp/patrimonioapi/resource/ItemResource.java
@@ -15,7 +15,7 @@ import com.herokuapp.patrimonioapi.model.Item;
 import com.herokuapp.patrimonioapi.repository.ItemRepository;
 
 @RestController
-@CrossOrigin(origins={"${parametro-origem}"})
+@CrossOrigin(origins={"https://patrimonio.now.sh","http://localhost:4200"})
 public class ItemResource {
 
 	@Autowired
